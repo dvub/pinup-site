8ce15fe0335b48996cadb198b65dde27693ba24f
kaya <84478450+dvub@users.noreply.github.com>
2023-09-10 22:23:44 -0700

    remove extra imports

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,13 +1,7 @@
 "use client";
-import Link from "next/link";
 import ImageDisplay from "../components/imageDisplay";
 import Navbar from "../components/navbar";
 import { comfortaa, cormorantGaramond } from "../layout";
-import Image from "next/image";
-import panelPic from "../../public/home/4panels.png";
-import gridPic from "../../public/home/grid pic.png";
-import frontPic from "../../public/home/square front.png";
-import backPic from "../../public/home/square back.png";
 import Footer from "../components/footer";
 import useWidth from "../components/hooks/useWidth";
 import Info from "./info";
