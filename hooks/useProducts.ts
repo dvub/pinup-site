1e6dd9cf55e2a31c2ecf428bdd5cc4e5b355cf79
dvub <dvubdevs@gmail.com>
2023-11-30 19:31:19 -0800

    refactor api routes

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -3,5 +3,5 @@ import useSWR from 'swr';
 
 export default function useProducts() {
 	const fetcher = (args: any) => axios.get(args).then((res) => res.data);
-	return useSWR('/products/api', fetcher);
+	return useSWR('/api/products', fetcher);
 }
