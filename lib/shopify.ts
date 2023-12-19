ba9d48deab013650c47ae57d07794a5ab5de448c
dvub <dvubdevs@gmail.com>
2023-12-19 15:40:37 -0800

    Added a comment

--- lib/shopify.ts
+++ lib/shopify.ts
@@ -1,5 +1,6 @@
 import Client from 'shopify-buy';
 export const client = Client.buildClient({
+  // these are the 2 environment variables you need to have set
 	storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_TOKEN!,
 	domain: process.env.SHOPIFY_DOMAIN!,
 	// TODO: specify
