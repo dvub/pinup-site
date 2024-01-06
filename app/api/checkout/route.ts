4b73d36164adf8b0d1e3348df91e3eb9fe428a93
dvub <dvubdevs@gmail.com>
2024-01-06 14:12:50 -0800

    bleh

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,6 +1,7 @@
 import generateClient from '@/lib/shopify';
 import { NextRequest } from 'next/server';
 import { Checkout } from 'shopify-buy';
+import Client from 'shopify-buy';
 
 // ROUTE HANDLER:
 // creates a checkout and returns the id
@@ -8,9 +9,9 @@ export async function GET(request: NextRequest) {
 	// dummy variable opts out of caching (lol stupid feature)
 	const header = request.headers;
 	// gen unoptimized client
-	const client = generateClient();
+	const client: Client = generateClient();
 
 	const checkout: Checkout = await client.checkout.create();
-	console.log(checkout.webUrl);
+	client.checkout.addLineItems('', []);
 	return Response.json({ checkoutId: checkout.id });
 }
