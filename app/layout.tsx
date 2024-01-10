68390ab6336fe8d32430bd94883aa170f9df7357
dvub <dvubdevs@gmail.com>
2024-01-10 15:28:42 -0800

    emergency fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,33 @@ import { hedvig } from '@/lib/fontLoader';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import { useRouter } from 'next/navigation';
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const isSiteOpen = process.env.SITE_OPEN!.toLowerCase() === 'true';
+	const Closed = () => {
+		return (
+			<div className='w-screen h-screen flex justify-center items-center'>
+				<div className='text-center'>
+					<h1 className='text-4xl'>pinup</h1>
+					<p className='py-1'>
+						The site isn&apos;t open right now. <br /> We&apos;ll be
+						back soon. Check out our socials to know about the next
+						drop.
+					</p>
+				</div>
+			</div>
+		);
+	};
 	return (
 		<html lang='en' className={hedvig.className}>
 			<body>
-				{children}
+				{!isSiteOpen && <Closed />}
+				{isSiteOpen && children}
 				<Analytics />
 			</body>
 		</html>
