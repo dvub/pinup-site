e9b7c645d60d29df787e016eeac74cf886b6c430
dvub <dvubdevs@gmail.com>
2024-02-02 12:04:56 -0800

    increase max items

--- app/vintage/page.tsx
+++ app/vintage/page.tsx
@@ -4,20 +4,20 @@ import { getVintageTag } from '@/lib/variables';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-	title: 'Vintage - Pinup',
-	description: '...',
+  title: 'Vintage - Pinup',
+  description: '...',
 };
 
 export const dynamic = 'force-dynamic';
 export default function Page() {
-	const tag = getVintageTag();
-	return (
-		<main>
-			<Navbar />
-			<div>
-				{/* <h1 className='m-2 text-xl'>showing: all vintage products</h1> */}
-				<ItemPanel type={tag} numItems={100} />
-			</div>
-		</main>
-	);
+  const tag = getVintageTag();
+  return (
+    <main>
+      <Navbar />
+      <div>
+        {/* <h1 className='m-2 text-xl'>showing: all vintage products</h1> */}
+        <ItemPanel type={tag} numItems={150} />
+      </div>
+    </main>
+  );
 }
